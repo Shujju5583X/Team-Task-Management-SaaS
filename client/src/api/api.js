77ba53742a0947c9f7a44e61c119a1849cf70f8f
Shujatullah <syedshujatullah29@gmail.com>
2025-11-27 09:35:39 +0530

    feat: Add initial API service setup with Axios and configure Vite for React development.

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://taskflow-backend-z65t.onrender.com/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
